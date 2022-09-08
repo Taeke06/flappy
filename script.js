6114af6417ae1f601715964f42d61bad8bb4e44e
Sennvdheiden <95484351+Sennvdheiden@users.noreply.github.com>
2022-09-08 08:12:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let colorlist = ['purple', 'yellow']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+}
+
+function draw() {
+  noStroke()
+  fill(random(colorlist));
+  ellipse(mouseX, mouseY, 25, 25);
+}
