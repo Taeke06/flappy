5f4c167d2fff9c57c0a864b13d0dc1c2b9408d7e
Taeke d <95484190+Taeke06@users.noreply.github.com>
2022-09-14 10:42:46 +0000

    begin balletje stuiteren met spatiebalk

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-class Rect {
-  constructor(x, y, w, h) {
+class Birb {
+  constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = w;
-    this.height = h;
-    this.color = "green";
+    this.vy = 0;
+    this.gravity = 0.2;
   }
 
-  drawRect() {
-    fill(this.color);
-    rect(this.x, this.y, this.width, this.height);
-    this.x += -3
+  drawBirb() {
+    fill("red");
+    circle(this.x, this.y, 10)
+
+    this.vy += this.gravity;
+
+    this.y += this.vy;
+
+    if (this.y > 380) {
+      this.vy = 0;
+      this.y = 380;
+    }
+
+    if (this.y < 0) {
+      this.vy = 0;
+      this.y = 0;
+    }
+  }
+}
+
+
+var birb;
+
+function setup() {
+  createCanvas(500, 400);
+
+  birb = new Birb(100, 200);
+
+}
+
+
+function draw() {
+  background(225);
+
+
+
+  birb.drawBirb();
+
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    birb.vy -= 5;
   }
 }
