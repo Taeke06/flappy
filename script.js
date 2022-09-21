f556de1cf9272b8fd657e61f8b0ef67bef5e0a3b
Sennvdheiden <95484351+Sennvdheiden@users.noreply.github.com>
2022-09-21 12:42:14 +0000

    de buizen gemaakt

--- script.js
+++ script.js
@@ -14,28 +14,44 @@ class Vliegtuig {
 
     this.y += this.vy;
 
-    if (this.y > 393   ) {
+    if (this.y > 323) {
       this.vy = 0;
-      this.y = 393;
+      this.y = 323;
     }
 
-    if (this.y < 3) {
+    if (this.y < 0) {
       this.vy = 0;
-      this.y = 3;
+      this.y = 0;
     }
   }
 }
 
+class rechthoek {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = "red";
+  }
 
+  drawrechthoek() {
+    fill(this.color);
+    rect(this.x, this.y, this.w, this.h);
+    this.x += -3;
+  }
+}
+
+var pipes = [];
 var vliegtuig;
 
 function preload() {
   img = loadImage("images/vliegtuig.png");
-  img2 = loadImage("images/achtergrond.jpg");
+  img2 = loadImage("images/achtergrondje.jpg");
 }
 
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(626, 368);
 
   vliegtuig = new Vliegtuig(100,200);
 
@@ -46,6 +62,27 @@ function setup() {
 
 function draw() {
   background(img2);
+
+  if (frameCount % 85 == 0) {
+
+
+    randomheight = random(height - 150);
+
+    pijp1 = new rechthoek(700, 0, 50, randomheight)
+    pijp2 = new rechthoek(700, randomheight + 150, 50, 300)
+
+    pipes.push(pijp1);
+    pipes.push(pijp2);
+
+    // remove unnessecary pipes
+    if (pipes.length > 6) {
+      pipes.splice(0, 2);
+    }
+  }
+
+    pipes.forEach((p) => {
+    p.drawrechthoek()
+  });
   
   vliegtuig.drawVliegtuig();
 
